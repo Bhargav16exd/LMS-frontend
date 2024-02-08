c109747e2d512d3342bc3970c205200408806867
Bhargav Mule <114209703+Bhargav16exd@users.noreply.github.com>
2024-02-08 19:32:23 +0530

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,15 +2,7 @@ import { configureStore } from "@reduxjs/toolkit";
 import AuthSlice from "./slices/AuthSlice";
 import CourseSlice from "./slices/courseSlice";
 import LectureSlice from "./slices/LectureSlice";
-import { logout } from "./slices/AuthSlice";
 
-const logoutOnUnload = (store) => (next) => (action) => {
-  window.addEventListener("beforeunload", () => {
-    store.dispatch(logout());
-  });
-
-  return next(action);
-};
 
 const store = configureStore({
   reducer: {
@@ -18,8 +10,6 @@ const store = configureStore({
     course: CourseSlice,
     lecture: LectureSlice,
   },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(logoutOnUnload),
   devTools: true,
 });
 
